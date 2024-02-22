1eaf3460c567b8f8cf72f4866981923a768e49bf
dean <1006393151@qq.com>
2024-02-22 23:58:53 +0800

    fix: check env

--- discord/discord.go
+++ discord/discord.go
@@ -34,7 +34,8 @@ var ProxyUrl = os.Getenv("PROXY_URL")
 var ChannelAutoDelTime = os.Getenv("CHANNEL_AUTO_DEL_TIME")
 var CozeBotStayActiveEnable = os.Getenv("COZE_BOT_STAY_ACTIVE_ENABLE")
 var UserAgent = os.Getenv("USER_AGENT")
-var UserAuthorizations = strings.Split(os.Getenv("USER_AUTHORIZATION"), ",")
+var UserAuthorization = os.Getenv("USER_AUTHORIZATION")
+var UserAuthorizations = strings.Split(UserAuthorization, ",")
 
 //var UserId = os.Getenv("USER_ID")
 
@@ -98,7 +99,7 @@ func StartBot(ctx context.Context, token string) {
 }
 
 func checkEnvVariable() {
-	if len(UserAuthorizations) == 0 {
+	if UserAuthorization == "" {
 		common.FatalLog("环境变量 USER_AUTHORIZATION 未设置")
 	}
 	//if UserId == "" {
