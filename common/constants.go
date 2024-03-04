0c7048d4b7a89e6adcbc07dd728e9d67efbaa434
dean <1006393151@qq.com>
2024-03-04 11:00:04 +0800

    doc

--- common/constants.go
+++ common/constants.go
@@ -16,7 +16,7 @@ var OnlyOpenaiApi = os.Getenv("ONLY_OPENAI_API")
 
 var DebugEnabled = os.Getenv("DEBUG") == "true"
 
-var Version = "v4.3.5" // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v4.3.6" // this hard coding will be replaced automatically when building, no need to manually change
 
 const (
 	RequestIdKey = "X-Request-Id"
