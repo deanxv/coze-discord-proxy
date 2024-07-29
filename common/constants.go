887e67c5614db8df8ffa0c897e6534eff2645c89
dean <1006393151@qq.com>
2024-07-29 18:08:07 +0800

    fix: model

--- common/constants.go
+++ common/constants.go
@@ -10,14 +10,31 @@ const (
 var ImgGeneratePrompt = "Please adhere strictly to my instructions below for the drawing task. If I do not provide a specific directive for drawing, create an image that corresponds to the text I have provided:\\n"
 
 var DefaultOpenaiModelList = []string{
-	"gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106", "gpt-3.5-turbo-0125",
-	"gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613",
+
+	"gpt-3.5-turbo",
+	"gpt-3.5-turbo-0125",
+	"gpt-3.5-turbo-0301",
+	"gpt-3.5-turbo-0613",
+	"gpt-3.5-turbo-1106",
+	"gpt-3.5-turbo-16k",
+	"gpt-3.5-turbo-16k-0613",
 	"gpt-3.5-turbo-instruct",
-	"gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-1106-preview", "gpt-4-0125-preview",
-	"gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613",
-	"gpt-4-turbo-preview", "gpt-4-turbo", "gpt-4-turbo-2024-04-09",
-	"gpt-4o", "gpt-4o-2024-05-13",
+	"gpt-4",
+	"gpt-4-0125-preview",
+	"gpt-4-0613",
+	"gpt-4-1106-preview",
+	"gpt-4-32k",
+	"gpt-4-32k-0314",
+	"gpt-4-32k-0613",
+	"gpt-4-turbo",
+	"gpt-4-turbo-2024-04-09",
+	"gpt-4-turbo-preview",
 	"gpt-4-vision-preview",
+	"gpt-4o",
+	"gpt-4o-2024-05-13",
+	"gpt-4o-mini",
+	"gpt-4o-mini-2024-07-18",
+
 	"dall-e-3",
 }
 
