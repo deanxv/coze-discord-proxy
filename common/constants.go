c05e29ded5837c2fcca44dd58b35acc2dfdf6c85
dean <1006393151@qq.com>
2024-10-31 13:23:42 +0800

    docs

--- common/constants.go
+++ common/constants.go
@@ -1,6 +1,6 @@
 package common
 
-var Version = "v4.6.2" // this hard coding will be replaced automatically when building, no need to manually change
+var Version = "v4.6.3" // this hard coding will be replaced automatically when building, no need to manually change
 
 const (
 	RequestIdKey = "X-Request-Id"
@@ -10,35 +10,43 @@ const (
 var ImgGeneratePrompt = "Please adhere strictly to my instructions below for the drawing task. If I do not provide a specific directive for drawing, create an image that corresponds to the text I have provided:\\n"
 
 var DefaultOpenaiModelList = []string{
-	"gpt-4-32k",
-	"gpt-4-32k-0613",
-
-	"gpt-4o",
-	"gpt-4o-2024-05-13",
-	"gpt-4o-2024-08-06",
-	"gpt-4o-mini",
-	"gpt-4o-mini-2024-07-18",
+	"gpt-3.5-turbo",
+	"gpt-3.5-turbo-1106",
+	"gpt-3.5-turbo-0125",
+	"gpt-3.5-turbo-16k",
+	"gpt-3.5-turbo-16k-0613",
+	"gpt-3.5-turbo-instruct",
 	"gpt-4",
 	"gpt-4-0613",
+	"gpt-4-1106-preview",
+	"gpt-4-0125-preview",
+	"gpt-4-turbo-preview",
 	"gpt-4-turbo",
 	"gpt-4-turbo-2024-04-09",
-	"gpt-4-1106-preview",
-	"gpt-4-turbo-preview",
-	"gpt-4-0125-preview",
-
-	"gpt-4-vision-preview",
-
-	"gpt-3.5-turbo",
-	"gpt-3.5-turbo-0125",
-	"gpt-3.5-turbo-1106",
-	"gpt-3.5-turbo-16k",
-	"gpt-3.5-turbo-instruction",
-
-	"chatgpt-4o-latest",
-
+	"gpt-4o",
+	"gpt-4o-2024-05-13",
+	"gpt-4o-mini",
+	"gpt-4o-mini-2024-07-18",
+	"text-embedding-ada-002",
+	"text-embedding-3-small",
+	"text-embedding-3-large",
+	"text-moderation-latest",
+	"text-moderation-stable",
+	"davinci-002",
+	"babbage-002",
+	"dall-e-2",
 	"dall-e-3",
+	"whisper-1",
+	"tts-1",
+	"tts-1-1106",
+	"tts-1-hd",
+	"tts-1-hd-1106",
+	"gpt-4o-2024-08-06",
 	"o1-mini",
 	"o1-preview",
+	"o1-preview-2024-09-12",
+	"chatgpt-4o-latest",
+	"o1-mini-2024-09-12",
 }
 
 var CozeErrorMessages = append(append(CozeOtherErrorMessages, CozeUserDailyLimitErrorMessages...), CozeCreatorDailyLimitErrorMessages...)
